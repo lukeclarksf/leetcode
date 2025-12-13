5656b790ee092bdd656db40794d4c1df066f977a
Luke Clark <lukeclarksf@gmail.com>
2025-12-14 00:17:16 +0100

    Commit

---
+++ scripts/create_files_md.py
@@ -0,0 +1,46 @@
+import os
+
+def create_md_files(start_num, end_num, content=None):
+    if start_num > end_num:
+        print("Error: The starting number must be less than or equal to the ending number.")
+        return
+
+    if content is None:
+        default_content = ""
+
+    print(f"Starting to create files from {start_num}.md to {end_num}.md...")
+    
+    for i in range(start_num, end_num + 1):
+        filename = f"../docs/{i}.md"
+        content = '```pyodide\n\
+{% \n\
+    include "../python/'+f"{i}"+'.py" \n\
+    preserve-includer-indent=false \n\
+%}\n\
+```\n\
+\n\
+```python\n\
+{% \n\
+    include "../python/'+f"{i}"+'.py" \n\
+    preserve-includer-indent=false \n\
+%}\n\
+```'
+        file_content = content if content is not None else default_content + str(i)
+
+        if not os.path.exists(filename):
+            try:
+                with open(filename, 'w') as f:
+                    f.write(file_content + "\n")
+
+            except IOError as e:
+                print(f"An error occurred while creating {filename}: {e}")
+        else:
+            print(f"File {i}.md exists!")
+
+    print("File creation complete!")
+
+START_NUMBER = 0
+END_NUMBER = 3772
+FILE_CONTENT = None
+
+create_md_files(START_NUMBER, END_NUMBER, FILE_CONTENT)
---
+++ scripts/create_files_py.py
@@ -0,0 +1,30 @@
+import os
+
+def create_python_files(start_num, end_num):
+    if start_num > end_num:
+        print("Error: The start number must be less than or equal to the end number.")
+        return
+
+    print(f"Starting to create files from {start_num}.md to {end_num}.md...")
+
+    for i in range(start_num, end_num + 1):
+        filename = f"../python/{i}.py"
+        if not os.path.exists(filename):
+            try:
+                with open(filename, 'w') as f:
+                    f.write(f"")
+                pass
+            except IOError as e:
+                print(f"Error creating file {filename}: {e}")
+                break
+        else:
+            print(f"File {i}.py exists!")
+
+    print("---")
+    print(f"✅ Finished! Created {end_num - start_num + 1} files.")
+    print("Files were created in the current working directory.")
+
+START_NUMBER = 0
+END_NUMBER = 3772
+
+create_python_files(START_NUMBER, END_NUMBER)
