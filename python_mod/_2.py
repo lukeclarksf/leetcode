1ddc8f73d4407ffadb185e2de32c24010b8064d5
Luke Clark <lukeclarksf@gmail.com>
2025-12-15 14:06:24 +0100

    Commit

--- python_mod/_2.py
+++ python_mod/_2.py
@@ -1,9 +1,3 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
     def addTwoNumbers(self, l1, l2):
         dummy_head = ListNode(0)
