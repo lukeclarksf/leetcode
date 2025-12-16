bdf91576df1c656736e8de6fa522009140e1f948
Luke Clark <lukeclarksf@gmail.com>
2025-12-16 01:18:51 +0100

    Commit

--- scripts/create_md.py
+++ scripts/create_md.py
@@ -1,10 +1,10 @@
 import os
 
-def create_python_files(start,end,part) -> None:
+def create_files(start,end,part,ext) -> None:
     print(f"Attempting to create 1000 empty .md files in {os.getcwd()}...")
 
     for i in range(start, end + 1):
-        filename = f"../python/{part}/docs/{i}.md"
+        filename = f"../python/{part}/docs/{i}.{ext}"
         
         if not os.path.exists(filename):
             try:
@@ -20,7 +20,7 @@ def create_python_files(start,end,part) -> None:
     print(f"Check your current directory for the new files (e.g., file_0001.py, file_1000.py).")
 
 # --- Execution ---
-create_python_files(1,1000,"part1")
-create_python_files(1001,2000,"part2")
-create_python_files(2001,3000,"part3")
-create_python_files(3001,3778,"part4")
+create_python_files(1,1000,"part1",'.md')
+create_python_files(1001,2000,"part2",'.md')
+create_python_files(2001,3000,"part3",'.md')
+create_python_files(3001,3778,"part4",'.md')
